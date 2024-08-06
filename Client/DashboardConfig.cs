3fe4a815381fe696c9abf53fea005706977dbac0
steviep <ig2bagd@yahoo.com>
2024-08-05 21:35:41 -0700

    Add dynamic component

---
+++ Client/DashboardConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorWasm1.Client;
+
+public class DashboardConfig
+{
+    public List<Panel> Panels { get; set; }
+}
+
+public class Panel
+{
+    public string Title { get; set; }
+    public string Component { get; set; }
+    public Dictionary<string, object> Parameters { get; set; } = new();
+}
