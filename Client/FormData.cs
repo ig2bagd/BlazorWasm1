662a00dddaee3cdde710b6432a73ec127fc99403
steviep <ig2bagd@yahoo.com>
2024-10-26 23:03:33 -0700

    Dynamic Form Example

---
+++ Client/FormData.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWasm1.Client;
+
+public class FormData
+{
+    public string InputValue { get; set; } = "John Smith";
+    public DateTime SelectedDate { get; set; } = DateTime.Today;
+    public string SelectedOption { get; set; } = "Option1";
+    public List<string> Options { get; set; } = new List<string>() { "Option1", "Option2", "Option3", "Option4", "Option5" };
+}
