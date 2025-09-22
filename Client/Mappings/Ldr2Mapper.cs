0177d1f36c7e3a950057190ababc74702477c3c0
steviep <ig2bagd@yahoo.com>
2025-09-21 20:11:37 -0700

    Add Mapperly v2

--- Client/Mappings/Ldr2Mapper.cs
+++ Client/Mappings/Ldr2Mapper.cs
@@ -1,7 +1,4 @@
 ﻿using Riok.Mapperly.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Globalization;
 
 namespace BlazorWasm1.Client.Mappings;
@@ -12,9 +9,11 @@ public partial class Ldr2Mapper
     public partial Destination Map(Source source);
     public partial Source Map(Destination destination);
 
-    // Custom logic for parsing
+    // Custom logic for parsing:  https://mapperly.riok.app/docs/configuration/user-implemented-methods/
     // Mapperly uses convention-based mapping and method signatures to integrate custom logic methods
-    // 	 Matching Signatures and Types: Mapperly looks for static methods in your mapper class whose signatures match the source and destination property types.
+    // 	 Matching Signatures and Types: Mapperly looks for methods in your mapper class whose signatures match the source and destination property types.
+    // If there are multiple user-implemented mapping methods suiting the given type-pair, by default, the first one is used.
+    [UserMapping(Default = true)]                                               // Not necessary, but explicit
     public static List<LdrItem2> StringToList(string input)
     {
         var ldrItems = new List<LdrItem2>();
@@ -45,6 +44,7 @@ public partial class Ldr2Mapper
     }
 
     // Custom logic for serialization
+    [UserMapping(Default = true)]                                               // Not necessary, but explicit
     public static string ListToString(List<LdrItem2> ldrItems)
     {
         if (ldrItems == null || ldrItems.Count == 0) return string.Empty;
